ffe94eaf0a7283a625ac18ffba65b9fca7fe5429
Zia Uddin <ziauddin@Zias-Mac-mini.lan>
2022-10-16 11:42:40 -0400

    homework 3

---
+++ Modules/Class6/homework-3.js
@@ -0,0 +1,59 @@
+// Due date: Oct-13 (Thu) eod
+
+/**
+ * Q1: Print the length of the country name (without using string.length or length property from string-library)
+ * 
+ */
+ let countryName = 'Japan';
+ // countryName.length;  <-- not allowed
+ console.log('\n\nusing slice function.....\n\n');
+ let slice0_5 = countryName.slice(0,5);
+ console.log(`sentence -> ${countryName}`);
+ console.log(`slice from index-0 to index-4(excluding index-5) -> '${slice0_5}'`);
+
+  /* Q2: find the number of words in the given sentence2
+  */
+ const sentence2 = "HeAlTh wAs EArlIer said To Be the AbILitY of the bOdY funcTiOnInG WElL.";
+ /* 
+    split(' ') -> [] -> length
+*/
+
+let numOfWords = sentence2.split(' ').length;
+
+console.log(`\n\nNumber of words -> ${numOfWords}\n\n`);
+
+ /**
+  * Q3: Create abbreviation for a 4-word sentence
+  * 
+  * 'you live Only once' -> YLOO
+  * 'you Never waLK ALONe' -> YNWA
+  */
+  const sentence3 = 'you Never waLK ALONe';
+  let abbr = '';
+  // code
+  console.log(`abbr -> ${abbr}`);     // abbr -> YNWA
+ 
+ /**
+  * Q4: Convert the string value in to Titlecase
+  * 
+  * 'you live Only once' -> You Live Only Once
+  * 'you Never waLK ALONe' -> You Never Walk Alone
+  * 'have a GrEAT daY' -> 'Have A Great Day'
+  */
+
+const sentence4 = 'have a GrEAT daY';
+let sentence4_Titlecase = '';
+
+const sentenceArray = sentence4.toLowerCase().split(' ');
+
+sentence4_Titlecase = sentence4_Titlecase + sentenceArray[0].charAt(0).toUpperCase() + sentenceArray[0].slice(1) + ' ';
+
+sentence4_Titlecase =  sentence4_Titlecase  + sentenceArray[1].charAt(0).toUpperCase() + sentenceArray[1].slice(1) + ' ';
+
+sentence4_Titlecase =  sentence4_Titlecase  + sentenceArray[2].charAt(0).toUpperCase() + sentenceArray[2].slice(1) + ' ';
+
+sentence4_Titlecase =  sentence4_Titlecase  + sentenceArray[3].charAt(0).toUpperCase() + sentenceArray[3].slice(1) + ' ';
+
+sentence4_Titlecase = sentence4_Titlecase.trim();
+
+console.log(`sentence4_Titlecase -> ${sentence4_Titlecase}`);
